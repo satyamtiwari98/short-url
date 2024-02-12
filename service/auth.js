c90a78e5924802ad7b2e5c7266177d862e003799
Satyam Tiwari <satyamtiwari1598@gmail.com>
2024-02-12 23:26:07 +0530

    JSONWEBTOKEN implemented

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,27 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "somethingveryunique123";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  //   sessionIdToUserMap.set(id, user);
+
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  sessionIdToUserMap.get(id);
+function getUser(token) {
+  //   sessionIdToUserMap.get(id);
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
